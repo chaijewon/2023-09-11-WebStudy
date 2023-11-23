a80d68dada6f07df0cd68edea2a9337b26403982
SIST <SIST@DESKTOP-IPQ8TQC>
2023-11-23 17:55:55 +0900

    111

--- HTMLProject_3/src/main/java/com/sist/view/FoodDetailServlet.java
+++ HTMLProject_3/src/main/java/com/sist/view/FoodDetailServlet.java
@@ -80,39 +80,7 @@ public class FoodDetailServlet extends HttpServlet {
 		out.println("</tr>");
 		
 		out.println("</table>");
-		out.println("<div style=\"height:30px\"></div>");
-		out.println("<table>");
-		out.println("<tr>");
-		out.println("<td align=center>");
-		String html="<div id=\"map\" style=\"width:100%;height:350px;\"></div>"
-				+ "        <script>"
-				+ "		var mapContainer = document.getElementById('map'),"
-				+ "		    mapOption = {"
-				+ "		        center: new kakao.maps.LatLng(33.450701, 126.570667),"
-				+ "		        level: 3 "
-				+ "		    };  "
-				+ "		"
-				+ "		var map = new kakao.maps.Map(mapContainer, mapOption); "
-				+ "		var geocoder = new kakao.maps.services.Geocoder();"
-				+ "		geocoder.addressSearch('"+vo.getAddress()+"', function(result, status) {"
-				+ "		     if (status === kakao.maps.services.Status.OK) {"
-				+ "		        var coords = new kakao.maps.LatLng(result[0].y, result[0].x);"
-				+ "		        var marker = new kakao.maps.Marker({"
-				+ "		            map: map,"
-				+ "		            position: coords"
-				+ "		        });"
-				+ "		        var infowindow = new kakao.maps.InfoWindow({"
-				+ "		            content: '<div style=\"width:150px;text-align:center;padding:6px 0;\">"+vo.getName()+"</div>'"
-				+ "		        });"
-				+ "		        infowindow.open(map, marker);"
-				+ "		        map.setCenter(coords);"
-				+ "		    } "
-				+ "		});    "
-				+ "		</script>";
-		out.println(html);
-		out.println("</td>");
-		out.println("</tr>");
-		out.println("</table>");
+		
 		out.println("</center>");
 		out.println("</body>");
 		out.println("</html>");
